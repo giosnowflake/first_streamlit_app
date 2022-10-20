2faaff77e845d8cbc656583ac660247ca31bbc19
giosnowflake <116045685+giosnowflake@users.noreply.github.com>
2022-10-20 13:39:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-import pandas
-import request
-import snowflake.connector
-from urllib.error import URLError
+#import pandas
+#import request
+#import snowflake.connector
+#from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -13,7 +13,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -29,7 +29,7 @@ streamlit.header('Fruityvice Fruit Adivce')
 fruit_choice = streamlit.text_input('What Fruit Would you like information about?', 'kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the response and normalizes it
@@ -38,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-streamlit.stop()
+#streamlit.stop()
 
 import snowflake.connector
 
